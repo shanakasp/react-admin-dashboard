40d3b118944f99f8f259fd78221854265c706f1c
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-02-25 14:19:18 +0530

    added 41 files

---
+++ src/scenes/dashboard/index.jsx
@@ -0,0 +1,130 @@
+import EmailIcon from "@mui/icons-material/Email";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import TrafficIcon from "@mui/icons-material/Traffic";
+import { Box, Typography, useTheme } from "@mui/material";
+import BarChart from "../../components/BarChart";
+import Header from "../../components/Header";
+import StatBox from "../../components/StatBox";
+import { tokens } from "../../theme";
+
+const Dashboard = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box m="20px">
+      {/* HEADER */}
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="Dashboard" subtitle="Welcome to your dashboard" />
+      </Box>
+
+      {/* GRID & CHARTS */}
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(12, 1fr)"
+        gridAutoRows="140px"
+        gap="20px"
+      >
+        {/* ROW 1 */}
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="12,361"
+            subtitle="Emails Sent"
+            progress="0.75"
+            increase="+14%"
+            icon={
+              <EmailIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="431,225"
+            subtitle="Sales Obtained"
+            progress="0.50"
+            increase="+21%"
+            icon={
+              <PointOfSaleIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="32,441"
+            subtitle="New Clients"
+            progress="0.30"
+            increase="+5%"
+            icon={
+              <PersonAddIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="1,325,134"
+            subtitle="Traffic Received"
+            progress="0.80"
+            increase="+43%"
+            icon={
+              <TrafficIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+
+        {/* ROW 2 */}
+
+        <Box
+          gridColumn="span 4"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+        >
+          <Typography
+            variant="h5"
+            fontWeight="600"
+            sx={{ padding: "30px 30px 0 30px" }}
+          >
+            Users Quantity
+          </Typography>
+          <Box height="250px" mt="-20px">
+            <BarChart isDashboard={true} />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Dashboard;
---
+++ src/scenes/global/Topbar.jsx
@@ -0,0 +1,113 @@
+import { ArrowDropDownCircleOutlined } from "@mui/icons-material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import React, { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+
+const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box
+      display="flex"
+      justifyContent="space-between"
+      backgroundColor={colors.primary[400]}
+      p={2}
+    >
+      {/* SEARCH BAR */}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <img
+          alt="profile-user"
+          width="100px"
+          height="100px"
+          src={`../../assets/logo.png`}
+          style={{ cursor: "pointer", borderRadius: "50%" }}
+        />
+      </Box>
+
+      {/* ICONS */}
+      <Box>
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlinedIcon style={{ fontSize: "2.2rem" }} />
+          ) : (
+            <LightModeOutlinedIcon style={{ fontSize: "2.2rem" }} />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlinedIcon style={{ fontSize: "2.2rem" }} />
+        </IconButton>
+
+        <IconButton
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          marginRight="20px"
+        >
+          <img
+            alt="profile-user"
+            width="50px"
+            height="50px"
+            src={`../../assets/user.png`}
+            style={{ cursor: "pointer", borderRadius: "50%" }}
+          />
+
+          <Typography
+            variant="h4"
+            color={colors.grey[100]}
+            fontWeight="bold"
+            sx={{ m: "10px" }}
+            onClick={handleClick}
+          >
+            Admin User <ArrowDropDownCircleOutlined />
+            <Typography variant="h5" color={colors.greenAccent[500]}>
+              Admin
+            </Typography>
+          </Typography>
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>
+              <IconButton>
+                <AccountCircleIcon />
+              </IconButton>
+              My Profile
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <IconButton>
+                <ExitToAppIcon />
+              </IconButton>
+              Logout
+            </MenuItem>
+          </Menu>
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
+
+export default Topbar;
